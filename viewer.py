8b9988cafcda67f4c118febe193d0ede5dc6e774
David Bonham <david@davidbonham.org.uk>
2021-12-18 13:49:20 +0000

    TkInter version
    Doesn't use a Frame, doesn't disable window processing

--- viewer.py
+++ viewer.py
@@ -1,123 +1,127 @@
 import os
 import sys
-import time
-import wx
+import tkinter
+from PIL import Image, ImageTk
 
-class ImagePanel(wx.Panel):
 
-    def __init__(self, parent, root):
-        super().__init__(parent)
-        self.file_path = None
-        self.root = root
-        self.images = []
-        self.image_index = None
-        screen_width, screen_height = wx.DisplaySize()
-        self.max_width = screen_width - 10
-        self.max_height = screen_height - 10
+class Viewer():
 
-        self.timer = wx.Timer(self)
-        self.Bind(wx.EVT_TIMER, self.update_image, self.timer)
+    def __init__(self, master):
 
-        img = wx.Image((10,10))
-        self.image_ctrl = wx.StaticBitmap(self, bitmap=wx.Bitmap(img))
-        main_sizer = wx.BoxSizer(wx.VERTICAL)
-        main_sizer.AddStretchSpacer()
-        main_sizer.Add(self.image_ctrl, 0, wx.ALIGN_CENTRE|wx.Centre)
-        main_sizer.AddStretchSpacer()
+        self.image_index = None      # Index of the current image
+        self.images = []             # The paths of images we know about
+        self.master = master
 
-        self.SetSizer(main_sizer)
-        self.SetBackgroundColour(wx.Colour(80, 80, 80))
-        main_sizer.Fit(parent)
-        self.Layout()
-        self.SetFocus()
+        # Set ourselves up as a full screen window
+        self.screen_w = master.winfo_screenwidth()
+        self.screen_h = master.winfo_screenheight()
 
-        self.Bind(wx.EVT_KEY_DOWN, self.on_key_down)
+        #Remove window manager decoration and processing of 'X' button 
+        #closing processing &c
+        #root.overrideredirect(1)
 
-        self.update_image(event=None)
-        self.timer.Start(250)
+        master.geometry("%dx%d+0+0" % (self.screen_w, self.screen_h))
+        master.title('Image Viewer')
+        master.configure(bg='#202020')
+        master.focus_set()    
+        self.multibind(master, ["<Escape>", 'q', 'Q'], self.on_escape)
+        self.multibind(master, ["<Left>", 'p', 'P'], self.on_left)
+        self.multibind(master, ['<Right>', 'n', 'N'], self.on_right)
+        self.multibind(master, ['<Home>', 'h', 'H'], self.on_home)
+        self.multibind(master, ['<End>', 'e', 'E'], self.on_end)
 
-    def on_key_down(self, event):
-        key_code = event.GetKeyCode()
-        print(key_code)
-        if key_code in (wx.WXK_ESCAPE, ord('Q')):
-            self.Parent.Close()
-        elif key_code in (ord('P'), wx.WXK_LEFT, wx.WXK_NUMPAD_LEFT):
-            self.back()
-        elif key_code in (ord('N'), wx.WXK_RIGHT, wx.WXK_NUMPAD_RIGHT):
-            self.next()
-        elif key_code in (ord('F'), wx.WXK_HOME):
-            self.first()
-        elif key_code in (ord('L'), wx.WXK_END):
-            self.last()
-        else:
-            event.Skip()
+        # We'll display our image in a label widget with no border
+        self.image_widget = tkinter.Label(master)
+        self.image_widget.pack()
+        self.image_widget.configure(bd=0, background='#808080')
 
-    def first(self):
-        self.image_index = 0
+    def goto_image(self, index):
+        self.image_index = index % len(self.images)
         self.load_image()
 
-    def last(self):
-        self.image_index = len(self.images) - 1
-        self.load_image()
+    def on_escape(self, event):
+        '''Quit the application'''
+        print('Quit')
+        event.widget.withdraw()
+        event.widget.quit()
 
-    def next(self):
-        self.image_index = (self.image_index + 1) % len(self.images)
-        self.load_image()
+    def on_home(self, _):
+        '''Go to the first image in the list'''
+        self.goto_image(0)
 
-    def back(self):
-        self.image_index = (self.image_index - 1) % len(self.images)
-        self.load_image()
+    def on_end(self, _):
+        '''Go to the final image in the list'''
+        self.goto_image(len(self.images) - 1)
 
-    def read_image(self, path):
-        while True:
-            try:
-                img = wx.Image(path, wx.BITMAP_TYPE_ANY)
-                return img, img.GetWidth(), img.GetHeight()
-            except:
-                print('retry', path)
-                time.sleep(0.25)
+
+    def on_left(self, _):
+        '''Go to the previous image, wrapping at the start'''
+        self.goto_image(self.image_index - 1)
+
+    def on_right(self, _):
+        '''Go to the next image, wrapping at the end'''
+        self.goto_image(self.image_index + 1)
+
+
+    def multibind(self, widget, events, handler):
+        '''Bind the handler to each event in the list'''
+        for item in events:
+            widget.bind(item, handler)
 
     def load_image(self):
+        '''Load the image with the current image index'''
+        if self.image_index == None: return
 
-        if self.image_index != None:
-            path = self.images[self.image_index]
+        # Get PIL to load the image
+        path = self.images[self.image_index]
+        pil_image = Image.open(path, 'r')
+        imgWidth, imgHeight = pil_image.size
+        self.master.title(f'Image Viewer - {imgWidth}x{imgHeight} - {path}')
 
-            img, w, h = self.read_image(path)
+        # Scale the image to fit the screen - the dimension that
+        # needs scaling most gives us the scale factor to use.
+        w_scale = imgWidth / self.screen_w
+        h_scale = imgHeight / self.screen_h
+        scale = max(w_scale, h_scale)
+        new_size = (int(imgWidth/scale), int(imgHeight/scale))
 
-            # Scale factors to apply to image to make each dimension fit
-            w_scale = w / self.max_width
-            h_scale = h / self.max_height
+        pil_image = pil_image.resize(new_size) #, Image.ANTIALIAS)
 
-            scale = max(w_scale, h_scale)
-            img = img.Scale(int(w / scale), int(h / scale))
-            self.image_ctrl.SetBitmap(wx.Bitmap(img))
-            self.Layout()
-            self.Refresh()
+        # We need to keep a reference to the photo image alive to 
+        # prevent it being garbage collected
+        self.current_image = ImageTk.PhotoImage(pil_image)
+        self.image_widget.configure(image=self.current_image)
+        print('loaded', self.image_index, self.images[self.image_index])
 
 
-    def update_image(self, event):
+    def updater(self):
+        '''Called regularly to see if new images have appeared on disk'''
 
-        paths = [os.path.join(self.root, file) for file in os.listdir(self.root) if file.lower().endswith('.jpg')]
+        # All of the files in the hot directory that end in .jpg 
+        paths = [os.path.join(sys.argv[1], file) for file in os.listdir(sys.argv[1]) if file.lower().endswith('.jpg')]
+
+        # Images that have appeared since we last looked
         new_images = [path for path in paths if path not in self.images and os.path.isfile(path)]
 
         if new_images != []:
+
+            # There are new images so whereever we were, move
+            # to the first new image and display it
             print('saw', new_images)
             self.image_index = len(self.images)
             self.images.extend(new_images)
             self.load_image()
 
+        '''We want to be called again'''
+        self.after(250, self.updater)
+
+import sys
+
+if __name__ == '__main__':    
+
+    tk = tkinter.Tk()
+    app = Viewer(master=tk)
+    tk.after(10, app.updater)
+    tk.mainloop()
 
 
-class MainFrame(wx.Frame):
-
-    def __init__(self, root):
-        super().__init__(None, title="Window Title")
-        panel = ImagePanel(self, root=root)
-        self.ShowFullScreen(True)
-
-
-
-if __name__ == '__main__':
-    app = wx.App(redirect=False)
-    frame = MainFrame(sys.argv[1])
-    app.MainLoop()
